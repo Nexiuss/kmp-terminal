e4b706e074911d4e21cd6946fedddf635603623a
nexiuss <nexiussunny@gmail.com>
2026-05-09 14:00:19 +0800

    init

---
+++ composeApp/src/jvmMain/kotlin/com/nexius/main.kt
@@ -0,0 +1,13 @@
+package com.nexius
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "kmp-terminal",
+    ) {
+        App()
+    }
+}
